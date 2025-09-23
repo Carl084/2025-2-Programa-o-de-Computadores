2f3944593061ded7f06ff098f7bd0d8adb86f944
carlz <carloswilliam.felix@gmail.com>
2025-09-23 14:14:22 -0300

    Adiciona exemplos 1 e 2.

---
+++ Ex2.py
@@ -0,0 +1,5 @@
+Nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade: ")  
+
+print("Olá, prazer em conhecer você,", Nome, "!", "Você tem", idade, "anos", "e você mora em", cidade, ". Espero que tenha um bom dia!")
