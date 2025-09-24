09fa152c9a653093cd1de848c6c017174a4967e5
CarlWilliam <Carloswilliam.felix@gmail.com>
2025-09-23 22:01:13 -0300

    Valores e Variáveis

---
+++ Ex3.py
@@ -0,0 +1,17 @@
+ano_atual = 2025
+ano_nascimento = int(input("Digite o seu ano de nascimento:"))
+vida_util = 120
+idade = ano_atual - ano_nascimento
+tempo_vida = vida_util - idade
+
+#print(idade)
+
+#print("Voce tem", idade, "anos.")
+
+#print("Voce tem" '+ str(idade) +' "anos.")
+
+#print('Voce nasceu em', ano_nascimento, 'e tem', idade, 'anos')
+
+#print(f'Voce tem apenas {tempo_vida} anos de vida')
+
+print(f'Voçê nasceu em {ano_nascimento} e tem {idade} anos')
