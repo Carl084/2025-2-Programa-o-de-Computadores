b77c77b794abe48b1dab858251c18cf99959a001
CarlWilliam <Carloswilliam.felix@gmail.com>
2025-09-24 21:27:36 -0300

    Organização e novos exercicios

---
+++ Exercicios/Ex1.py
@@ -0,0 +1,11 @@
+#Pedir a medida em float
+raio = float(input('Me de a medida do raio:'))
+
+#Numero Pi
+pi = 3.1416
+
+#Calculo da área
+area = pi*raio**2
+
+#Print do resultado
+print(f'A área do circulo é {area:.2f}.')
---
+++ Exercicios/Ex2.py
@@ -0,0 +1,10 @@
+#Pedindo as medidas de base maior, a menos e altura
+Lbase = float(input('Me de a medida da base maior:'))
+Sbase = float(input('Me de a medida da base menor:'))
+Height = float(input('Me de a medida da altura:'))
+
+#Calculo da área
+area = ((Lbase+Sbase)*Height)/ 2
+
+#Print do resultado
+print(f'A área do trapezio é {area:.2f}.')
