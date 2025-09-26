d673c5242c65f2dab2dd34c0e70d113571e8f25b
Carl2 <carloswilliam.felix@gmail.com>
2025-09-25 21:18:38 -0300

    Real_to_Dollar

---
+++ Exercicios/25.09/Conversor_moeda.py
@@ -0,0 +1,9 @@
+#Valor a converter para o dolar.
+valueConvert = float(input('Insira o valor a converter em reais:'))
+DolarValue = float(input('Insira o valor do dola atual:'))
+
+#Calcula para a converção
+ResultDiv = valueConvert/DolarValue
+
+#Print do resultado
+print(f'Você tinha {valueConvert} reais, que é {ResultDiv:.3f} dolares.')
