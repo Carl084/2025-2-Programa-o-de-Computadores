5455f5eb70d36052fcda288271f3e8b3acc6b6ac
CarlWilliam <Carloswilliam.felix@gmail.com>
2025-09-29 19:09:51 -0300

    upname

--- Exercicios/22-24.09/Conversor_temperatura.py
+++ Exercicios/22-24.09/Conversor_temperatura.py
@@ -1,5 +1,8 @@
+#Peço a temporatura em C°
 temp = float(input('Informe a temperatura em Celsius:'))
 
+#Faço o calculo de converção
 tempF = (temp * 9/5) + 32
 
+#Informo a temperatura
 print(f'A temperatura em Fahrenheit é {tempF:.1f}')
---
+++ Exercicios/Ex3.py
@@ -0,0 +1,7 @@
+
+valorA = int(input('Informe o primeiro valor inteiro:'))
+valorB = int(input('Informe o segundo valor inteiro:'))
+
+media = (valorA + valorB)/2
+
+print(f'A sua media é {media}')
