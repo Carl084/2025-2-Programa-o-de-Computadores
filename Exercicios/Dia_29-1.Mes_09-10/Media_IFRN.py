ab61a31b91e84a3d260081c9bed66a49fea38926
CarlWilliam <Carloswilliam.felix@gmail.com>
2025-09-30 21:55:22 -0300

    updata

--- Exercicios/Dia_29-1.Mes_09-10/Media_IFRN.py
+++ Exercicios/Dia_29-1.Mes_09-10/Media_IFRN.py
@@ -1,16 +1,30 @@
-#Pedindo as notas
-Nota1 = int(input('Informe a nota da etapa 1:'))
-Nota2 = int(input('Informe a nota da etapa 2:'))
-#Calculando a media das notas
-Media = (Nota1*2 + Nota2*3)/5
-#Informando o resultado da nota
-print(f'Sua media na Diatinf é {Media}')
-#Caso seja aprovado
-if Media>=60:
-    print('Você está aprovado')
-#Caso esteja em recuperação
-elif Media>=20:
-    print('Você está em recuperação')
-#Caso seja reprovado
-else:
-    print('Você está reprovado')
+while True:
+    #Pedindo as notas
+    note1 = int(input('Informe a nota da etapa 1:'))
+    note2 = int(input('Informe a nota da etapa 2:'))
+    if not note1>=0 and not note2<=100 and not note2>=0 and not note2<=100:
+        print('Nota 1 ou 2 ultrapassou o limite')
+    #Pedindo a carga horaria e faltas do aluno
+    workload = int(input('Infome a carga horaria da diciplina (h/a):'))
+    lack = int(input('Informe a quantidade de faltas do aluno:'))
+    if not workload>=0:
+        print('Uma carga horaria negativa???')
+    if not lack>=0 and not lack>workload:
+        print('A quantidade de faltas passou o limite existente.')
+    #Calculando a media das notas
+    average = int((note1*2 + note2*3)/5)
+    frequency = int((1-(lack/workload))*100)
+    #Informando o resultado da nota
+    print(f'Sua media na Diatinf é {average} e tem uma frequencia de {frequency}%.')
+    #Caso seja aprovado
+    if average>=60 and frequency>=75:
+        print('Você está aprovado')
+        break
+    #Caso esteja em recuperação
+    elif average>=20 and frequency>=75:
+        print('Você está em recuperação')
+        break
+    #Caso seja reprovado
+    else:
+        print('Você está reprovado')
+        break
