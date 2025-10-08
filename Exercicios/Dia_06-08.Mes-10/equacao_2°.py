36d0ca9b2e99cf5188e07ea5024eeb136a4c8227
CarlWilliam <Carloswilliam.felix@gmail.com>
2025-10-07 21:49:08 -0300

    updata

--- Exercicios/Dia_06-08.Mes-10/equacao_2°.py
+++ Exercicios/Dia_06-08.Mes-10/equacao_2°.py
@@ -1,9 +1,9 @@
 import sys
 try:
     #Pedir os valores
-    a = int(input('Digite o valor A:'))
-    b = int(input('Digite o valor B:'))
-    c = int(input('Digite o valor C:'))
+    a = int(input('Digite o valor "a":'))
+    b = int(input('Digite o valor "b":'))
+    c = int(input('Digite o valor "c":'))
 #Verificar erros
 except ValueError:
     sys.exit('ERRO: Informe apenas números válidos para a, b e c')
@@ -11,7 +11,7 @@ except Exception as strErro:
    sys.exit(f'EXCEÇÃO: {sys.exc_info()[0]} -> {strErro}')
 #Verificar erro do a sendo 0
 if a == 0:
-    sys.exit('ERRO: a equação não é de 2° grau.')
+    sys.exit('ERRO: A equação não é de 2° grau.')
 #calculo do delta
 delta = b**2-4*a*c
 print(f'\nDelta (discriminante) = {delta}')
