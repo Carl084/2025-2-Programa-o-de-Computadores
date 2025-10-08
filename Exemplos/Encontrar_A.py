f68fd1a0ea922d07b49d8f00a60313f472550074
CarlosCasa <Carloswilliam.felix@gmail.com>
2025-10-08 10:56:11 -0300

    updata

--- Exemplos/Encontrar_A.py
+++ Exemplos/Encontrar_A.py
@@ -1,13 +1,18 @@
-#Peço a frase a encontrar as letras A
+#Solicito a frase a encontrar as letras A
 frase = input('Digite uma frase:')
+
 #Transformo a frase em menuscula
 frase = frase.lower()
+
 #Conto a quantidade de A
 quantidade = frase.count('a')
+
 #Encontro o primeiro A
 primeira = frase.find('a') + 1
+
 #Encontro o segundo A
 ultima = frase.rfind('a') + 1
+
 #Apresento a quantidade, a 1° e ultimo A
 print(f'A letra "A" apareceu {quantidade} vezes.')
 print(f'A primeira vez que apareceu foi na posição {primeira}')
