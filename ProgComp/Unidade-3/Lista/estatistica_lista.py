d5fa699cc0c832aa51c8d5577823bfb9f3fadeeb
Carl084 <Carloswilliam@gmail.com>
2025-11-26 21:15:45 -0300

    atualização da atividade

--- ProgComp/Unidade-3/Lista/estatistica_lista.py
+++ ProgComp/Unidade-3/Lista/estatistica_lista.py
@@ -30,19 +30,28 @@ else:
             List_values.append(IntValue_random)
             i += 1
     
+    average = sum(List_values)/len(List_values)
+    square_deviation = [(x - average)**2 for x in List_values]
+
     List_values_salaries = sorted(List_values)
     size = len(List_values_salaries)
     
+    variance = sum(square_deviation)/sum(List_values)
+
     if len(List_values) % 2 == 0:
         center1 = List_values_salaries[size//2-1]
         center2 = List_values_salaries[size//2]
         median = (center1)+(center2)/2
     else:
         median = List_values_salaries[size//2]
-        
+    
+
     print(f'Lista gerada: {List_values}')
     print(f'Lista orden.: {List_values_salaries}')
     print(f'Soma dos valores na lista: {sum(List_values)}')
-    print(f'Média da lista: {sum(List_values)/len(List_values)}')
+    print(f'Média da lista: {average}')
+    print(f'O Valor maximo: {max(List_values_salaries)}')
+    print(f'O Valor minimo: {min(List_values_salaries)}')
     print(f'A mediana: {median}')
-    
+    print(f'Variância: {variance}')
+    print(f'Desvio padrão: {variance**0.5}')
