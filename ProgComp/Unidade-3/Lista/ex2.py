02cde1b5b15a91b697d96185038c01c5c40252c0
Carl084 <Carloswilliam@gmail.com>
2025-12-01 19:44:54 -0300

    conclusão exercicio 2

--- ProgComp/Unidade-3/Lista/ex2.py
+++ ProgComp/Unidade-3/Lista/ex2.py
@@ -32,11 +32,35 @@ lstNotas1 = []
 lstNotas2 = []
 lstMedias = []
 
-while True:
-   input('Digite o nome do aluno: ').append(lstNomes)
-   Nota1 = int(input('Digite a nota da etapa 1: ')).append(lstNotas1)
-   Nota2 = int(input('Digite a nota da etapa 2: ')).append(lstNotas2)
-   ((Nota1 * 2) + (Nota2 * 3) / 5).append(lstMedias)
-   print('Para? (S ou N)')
-   input('Resposta: ')
-   if 
+intAlunos = 1
+while intAlunos <= 5:
+   strNome = input(f'Digite o nome do {intAlunos}º aluno: ')
+   try:
+      intNota1 = int(input('Digite a nota da etapa 1: '))
+      intNota2 = int(input('Digite a nota da etapa 2: '))
+   except ValueError:
+      print('Erro: Digite um valor inteiro para a nota!')
+      continue
+   else:
+      if (intNota1 < 0 or intNota1 > 100):
+         print('ERRO: A nota da Etapa 1 deve estar entre 0 e 100.')
+         continue
+      if (intNota2 < 0 or intNota2 > 100):
+         print('ERRO: A nota da Etapa 2 deve estar entre 0 e 100.')
+         continue
+   
+   intMedia = round(((intNota1 * 2) + (intNota2 * 3)) / 5)
+   
+   lstNomes.append(strNome)
+   lstNotas1.append(intNota1)
+   lstNotas2.append(intNota2)
+   lstMedias.append(intMedia)
+   
+   intAlunos += 1
+   
+print(f'{'Nome do Aluno':<15} {'Etapa 1':>8} {'Etapa 2':>8} {'Média':>8}')
+print('-' * 60)
+
+for i in range(5):
+   print(f'{lstNomes[i]:<15} {lstNotas1[i]:>8} {lstNotas2[i]:>8} {lstMedias[i]:>8}')
+print('-' * 60)
